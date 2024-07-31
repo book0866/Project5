6ee2126ef420495aa94fad5c07ec62b1a21c48ed
Book <wodls66@naver.com>
2024-07-31 13:22:24 +0900

    29일차~

--- src/reference/quiz/Quiz1.java
+++ src/reference/quiz/Quiz1.java
@@ -4,63 +4,47 @@ public class Quiz1 {
 
 	public static void main(String[] args) {
 		
-		// 회원을 생성하고, 회원이 대여한 책 3권을 추가하세요.
-		Book book1 = new Book("책1", 10000, "북판사", 100);
-		Book book2 = new Book("책2", 20000, "북판사", 200);
-		Book book3 = new Book("책3", 30000, "북판사", 300);
+		//책 3권 생성
+		Book book1 = new Book("나미야잡화점", 35000, "누리출판사", 150);
+		Book book2 = new Book("아몬드", 18000, "별곰자리출판사", 210);
+		Book book3 = new Book("기억전달자", 10000, "별곰자리출판사", 180);
 		
-		Member member = new Member(101, "이름", 책1, 책2, 책3);
-		
+		//사람 객체 생성
+		Member member = new Member(1001, "둘리", book1, book2, book3);
 	}
-
 }
 
-
-//책(Book)을 정의하는 클래스를 만드세요.
-//속성: 제목, 가격, 출판사, 페이지 수
+// 도서 클래스
 class Book {
+	String title; //제목
+	int price; //가격
+	String company; //출판사
+	int page; //페이지수
 	
-	String bookTitle;
-	int bookPrice;
-	String bookCompany;
-	int bookPage;
-	
-	
-	public Book(String bookTitle, int bookPrice, String bookCompany, int bookPage) {
-		super();
-		this.bookTitle = bookTitle;
-		this.bookPrice = bookPrice;
-		this.bookCompany = bookCompany;
-		this.bookPage = bookPage;
+	public Book(String title, int price, String company, int page) {
+		this.title = title;
+		this.price = price;
+		this.company = company;
+		this.page = page;
 	}
-	
 }
 
-
-// 도서관이 회원(Member)을 정의하는 클래스를 만드세요.
-// 속성: 회원번호, 이름, 대여한 책1, 책2, 책3
-
+// 도서관 회원 클래스
 class Member {
-	
-	int memberNo;
-	String memberName;
-	Book book1;
-	Book book2;
-	Book book3;
-	
-	
-	public Member(int memberNo, String memberName, Book book1, Book book2, Book book3) {
-		super();
+	int memberNo; //회원번호
+	String name; //이름
+	Book book1; //대여한책1
+	Book book2; //대여한책2
+	Book book3; //대여한책3
+
+	public Member(int memberNo, String name, Book book1, Book book2, Book book3) {
 		this.memberNo = memberNo;
-		this.memberName = memberName;
+		this.name = name;
 		this.book1 = book1;
 		this.book2 = book2;
 		this.book3 = book3;
 	}
-	
 }
-
-
 
 
 
